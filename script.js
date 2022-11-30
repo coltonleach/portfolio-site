494684400e994d1ffc3fd229a2a122fa56124b11
Colton Leach <leach.colton@hotmail.com>
2022-11-30 14:59:53 -0600

    added a feature to animate certain items in whenever they are in the viewport

---
+++ script.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll('.inactive')
+
+const options = {
+  root: null, //viewport
+  threshold: 0.2, //Percentage of element required to be visisble in viewport for observer to fire
+  rootMargin: '-20%',
+}
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.remove('inactive')
+  })
+}, options)
+
+sections.forEach((section) => observer.observe(section))
