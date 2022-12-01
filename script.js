b643c0e299477e83668bdbbbbcd27988268a451d
Colton Leach <leach.colton@hotmail.com>
2022-11-30 23:01:21 -0600

    fixed the intersection observer settings

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const sections = document.querySelectorAll('.inactive')
 
 const options = {
   root: null, //viewport
-  threshold: 0, //Percentage of element required to be visisble in viewport for observer to fire
-  rootMargin: '-20%',
+  threshold: 0.3, //Percentage of element required to be visisble in viewport for observer to fire
+  
 }
 
 const observer = new IntersectionObserver((entries, observer) => {
