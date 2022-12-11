be6804e1d6f53af2dfa4baa93bfc25244bf86cf1
Colton Leach <leach.colton@hotmail.com>
2022-12-11 09:28:48 -0600

    unobserved the elements after the observer fires for cleanup

--- script.js
+++ script.js
@@ -3,14 +3,18 @@ const sections = document.querySelectorAll('.inactive')
 const options = {
   root: null, //viewport
   threshold: 0.3, //Percentage of element required to be visisble in viewport for observer to fire
-  
 }
 
-const observer = new IntersectionObserver((entries, observer) => {
+const observerCallback = (entries, observer) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting)
+    if (entry.isIntersecting) {
+      console.log('intersecting')
       entry.target.classList.remove('inactive', 'left', 'right')
+      observer.unobserve(entry.target)
+    }
   })
-}, options)
+}
+
+const observer = new IntersectionObserver(observerCallback, options)
 
 sections.forEach((section) => observer.observe(section))
